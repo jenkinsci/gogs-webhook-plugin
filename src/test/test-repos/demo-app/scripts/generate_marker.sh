1dc3cfcd72ec2c1dd1c98d697dd1035283e307e9
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2017-05-14 12:14:30 +0200

    Improve execution marker and result publication

--- src/test/test-repos/demo-app/scripts/generate_marker.sh
+++ src/test/test-repos/demo-app/scripts/generate_marker.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Hello world !"
+# build unique filename
+marker_file="marker.txt"
+
+# Dump some useful info in the marker file if available
+echo "# --== ${marker_file} ==--" > ${marker_file}
+echo " "  >> ${marker_file}
+
+#
+echo "BUILD_NUMBER=${BUILD_NUMBER:-nihil}" >> ${marker_file}
+echo "GIT_COMMIT=${GIT_COMMIT:-nihil}" >> ${marker_file}
+echo "GIT_URL=${GIT_URL:-nihil}" >> ${marker_file}
+echo "GIT_BRANCH=${GIT_BRANCH:-nihil}" >> ${marker_file}
+echo "JOB_NAME=${JOB_NAME:-nihil}" >> ${marker_file}
+echo "WORKSPACE=${WORKSPACE:-nihil}" >> ${marker_file}
